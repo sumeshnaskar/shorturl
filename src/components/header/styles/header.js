c7b0643f99bdc138c1d7f3782f4b463cf3dd3577
Sumesh <sumeshnaskar27@gmail.com>
2021-09-04 03:58:44 +0530

    small screen menu

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -5,17 +5,92 @@ export const Container = styled.div`
     margin: 0 auto;
 `
 
-export const NavBar = styled.nav`
-    :last-of-type{
-        position: absolute;
-        right: 2em;
-    }
+export const Group = styled.div`
 
-    @media(max-width:700px){
-        display: none;
+
+    @media(min-width:700px){
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
     }
 `
 
+export const NavGroup = styled.div`
+    display: flex;  
+    justify-content: space-between;
+    align-items: flex-end; 
+
+    @media(min-width:700px){
+            
+        
+    }
+`
+
+export const NavBar = styled.nav`
+    background-color: hsl(257, 27%, 26%);
+    width: 90%;
+    border-radius: 10px;
+    position: absolute;
+    top: 3em; 
+
+    @media(min-width:700px){
+        background-color: #fff;
+        display: flex;
+        position: static;
+        justify-content: space-between;
+    }
+`
+export const NavList = styled.ul`
+    text-align: center;
+    padding: 0;
+    margin: 0.5em 0;
+    
+
+    @media(min-width:700px){
+        display: flex;
+        align-items: flex-end;
+        justify-content: space-between;
+        margin: 0;
+
+        
+    }
+
+    
+`
+export const NavItem = styled.li`
+    list-style-type: none;
+    font-weight: 500;
+    color: hsl(0, 0%, 75%);
+    font-size: 0.8rem;
+    padding: 0.25em;
+    cursor: pointer;
+    margin-right: 2em;
+
+    &:hover,:focus{
+        color: #fff;
+    }
+
+    @media(min-width:700px){
+        display: flex; 
+        padding: 0;
+
+        &:hover,:focus{
+            color: #000;
+        }
+    }
+`
+
+export const Break = styled.div`
+    height: 0.5px;
+    background-color: hsl(0, 0%, 75%);
+    width: 80%;
+    margin: 0 auto;
+
+    @media(min-width:700px){
+        display: none;
+    }
+
+`
 export const Inner = styled.div`
     display: flex;
     flex-direction: column;
@@ -36,20 +111,9 @@ export const Inner = styled.div`
     }    
 `
 
-export const Group = styled.div`
-    display: flex;
-    justify-content: space-between;
 
-    @media(max-width:700px){
-        flex-direction: column;
-    }
-`
 
-export const NavGroup = styled.div`
-    display: flex;
-    justify-content: flex-between;
-    align-items: flex-end;
-`
+
 
 export const Logo = styled.img` 
     margin-top: 1em;
@@ -81,6 +145,7 @@ export const Button = styled.button`
     margin-left: 0;
     font-weight: 700;
     font-size: 0.8rem;
+    margin-right: 2em;
 
     &:hover,:focus{
         background-color: hsl(180, 66%, 75%);
@@ -98,14 +163,14 @@ export const Button = styled.button`
 `
 
 export const Hamburger = styled.div`
-  display: none;
+    display: block;
+    cursor: pointer;
+    position: relative;
+    right: 0;
+    margin-right: 0.5em;
 
-  @media(max-width:700px){
-      display: block;
-      cursor: pointer;
-      position: absolute;
-      right: 0;
-      margin-right: 0.5em;
+  @media(min-width:700px){
+        
     }
 
 `
@@ -116,6 +181,11 @@ export const Bar = styled.span`
     height: 3px;
     margin: 5px;
     background-color: hsl(0, 0%, 75%);
+
+    @media(min-width:700px){
+        visibility: hidden;
+        cursor: none;
+    }
 
 `
 
--- src/container/header.js
+++ src/container/header.js
@@ -7,17 +7,24 @@ export function HeaderContainer(){
             <Header>
                 <Header.NavGroup>
                     <Header.Logo src = {process.env.PUBLIC_URL + "/images/logo.svg"} alt="Shorty logo"/>
-                    <Header.NavBar> 
-                        <Header.Link href="#">Features</Header.Link>
-                        <Header.Link href="#">Pricing</Header.Link>
-                        <Header.Link href="#">Resources</Header.Link>
-                    </Header.NavBar>
+                    <Header.Hamburger/>
                     <Header.NavBar>
-                        <Header.Link href="#">Login</Header.Link>
-                        <Header.Button>Sign Up</Header.Button>                                
-                    </Header.NavBar>         
-                    <Header.Hamburger />          
+                        <Header.NavList>
+                            <Header.NavItem>Features</Header.NavItem>
+                            <Header.NavItem>Pricing</Header.NavItem>
+                            <Header.NavItem>Resources</Header.NavItem>
+                        </Header.NavList>
+                        <Header.Break/>
+                        <Header.NavList>
+                            <Header.NavItem>Login</Header.NavItem>
+                            <Header.Button>Sign Up</Header.Button>
+                        </Header.NavList>
+                    </Header.NavBar>
+                    
+                    
+                             
                 </Header.NavGroup>
+                
                 <Header.Group>
                     <Header.Inner>                    
                         <Header.Title>More than just shorter links</Header.Title>
